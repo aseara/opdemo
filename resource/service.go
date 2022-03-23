15f6fcfa303e6c49a57694e3730061ecf3ad77f0
aseara <aseara@163.com>
2022-03-23 17:50:59 +0800

    实现业务逻辑

---
+++ resource/service.go
@@ -0,0 +1,36 @@
+package resource
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	appv1 "github.com/aseara/demo-operator/api/v1"
+)
+
+func NewService(app *appv1.AppService) *corev1.Service {
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: app.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(app, schema.GroupVersionKind{
+					Group:   appv1.GroupVersion.Group,
+					Version: appv1.GroupVersion.Version,
+					Kind:    "AppService",
+				}),
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Type:  corev1.ServiceTypeNodePort,
+			Ports: app.Spec.Ports,
+			Selector: map[string]string{
+				"app": app.Name,
+			},
+		},
+	}
+}
